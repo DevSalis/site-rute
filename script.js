d9c05297cae20deffbf836ded8e15000546f4e74
MrSalis <philip.cost@hotmail.com>
2024-03-25 02:40:51 -0300

    carrossel ajustado

--- script.js
+++ script.js
@@ -20,3 +20,33 @@ document.addEventListener('DOMContentLoaded', function () {
         sideScroll(container, 'left', 25, 100, 10);
     });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    let currentIndex = 0;
+    const images = document.querySelectorAll('.imgs-portfolio');
+    const imageWidth = document.querySelector('.imgs-portfolio').clientWidth; // Largura de uma imagem
+    const boxSlideCarrossel = document.getElementById('box-slide-carrossel');
+
+    const updateCarousel = () => {
+        const moveX = -(imageWidth * currentIndex);
+        boxSlideCarrossel.style.transform = `translateX(${moveX}px)`;
+    };
+
+    document.getElementById('nextBtn').addEventListener('click', function () {
+        if (currentIndex < images.length - 3) { // Certifica-se de não mover demais para a direita
+            currentIndex++;
+        } else {
+            currentIndex = 0; // Volta ao início para o efeito de loop
+        }
+        updateCarousel();
+    });
+
+    document.getElementById('prevBtn').addEventListener('click', function () {
+        if (currentIndex > 0) { // Certifica-se de não mover demais para a esquerda
+            currentIndex--;
+        } else {
+            currentIndex = images.length - 3; // Vai para o final se estiver no início
+        }
+        updateCarousel();
+    });
+});
